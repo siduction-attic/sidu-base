0f9577e43c6ff637a6f9e5be996e81247a07fc0a
Alf Gaida <agaida@siduction.org>
2014-08-31 17:24:37 +0200

    new automount-control.sh

--- backend/automount-control.sh
+++ backend/automount-control.sh
@@ -3,32 +3,42 @@
 #set -x
 date "+%Y.%m.%d %H:%M:%S: $1" >>/tmp/automount.log
 
-if [ ! -x /usr/bin/gsettings ] ; then
-	exit 2
-else
-	if [ -f /etc/default/distro ]; then
-		. /etc/default/distro
-	fi
+[[ ! -x /usr/bin/gsettings ]] && exit 2
 
-	#if [ ! -f /etc/default/fll-gnome-desktop ]; then
-		session=$(pgrep -u "${FLL_LIVE_USER}" session)
-		export $(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$session/environ)
-		#test -z $VERBOSE || echo "DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS"
-	#fi
-
-	case "$1" in
-		disabled)
-			su ${FLL_LIVE_USER} -c "gsettings set org.gnome.desktop.media-handling automount-open false"
-		;;
-		enabled)
-			su ${FLL_LIVE_USER} -c "gsettings set org.gnome.desktop.media-handling automount-open true"
-		;;
-		*)
-			echo "automount-control.sh called with unknown argument \`$1'" >&2
-			exit 1
-		;;
-	esac
+if [ -f /etc/default/distro ]; then
+    . /etc/default/distro
 fi
+
+#if [ ! -f /etc/default/fll-gnome-desktop ]; then
+session=$(pgrep -u "${FLL_LIVE_USER}" session)
+export $(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$session/environ)
+#test -z $VERBOSE || echo "DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS"
+#fi
+
+
+case "$1" in
+    enabled)
+	ENABLE_AUTOMOUNT=true
+        ;;
+    disabled)
+	ENABLE_AUTOMOUNT=false
+        ;;
+    *)
+        echo "automount-control.sh called with unknown argument \`$1'" >&2
+        exit 1
+        ;;
+esac
+
+
+case "$FLAVOUR" in
+    Gnome)
+        su ${FLL_LIVE_USER} -c "gsettings set org.gnome.desktop.media-handling automount-open $ENABLE_AUTOMOUNT"
+        ;;
+    Cinnamon)
+        ###su ${FLL_LIVE_USER} -c "gsettings set org.gnome.desktop.media-handling automount-open true"
+	;;
+esac
+
 #set +x
 
 
